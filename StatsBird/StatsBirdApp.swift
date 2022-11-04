489b65c009a5e4339c05f3515a87d9789bb443cd
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-11-04 15:13:53 +0900

    first commit

---
+++ StatsBird/StatsBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsBirdApp.swift
+//  StatsBird
+//
+//  Created by Fus1onDev on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct StatsBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
